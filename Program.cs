af8714a80d605d4eb8b8bdf0d16cb0b37c4694e1
filipw <wojcieszyn@gmail.com>
2023-04-21 21:04:47 +0200

    added AnsiConsole

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Xml.Linq;
 using Azure;
 using Azure.AI.OpenAI;
+using Spectre.Console;
 
 var azureOpenAiServiceEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_SERVICE_ENDPOINT");
 var azureOpenAiServiceKey = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
@@ -26,23 +27,66 @@ else
     return;
 }
 
+var results = await GetAIRatings(azureOpenAiDeploymentName, feedItems);
 
-var client = new OpenAIClient(new Uri(azureOpenAiServiceEndpoint), new AzureKeyCredential(azureOpenAiServiceKey));
-
-var completionsOptions = new CompletionsOptions()
+foreach (var rated in results) 
 {
-    Temperature = 0,
-    MaxTokens = 2000,
-    NucleusSamplingFactor = 1,
-    FrequencyPenalty = 0,
-    PresencePenalty = 0,
-    GenerationSampleCount = 1,
-};
+    var feedItem = feedItems.FirstOrDefault(x => x.Id == rated.Id);
+    if (feedItem != null)
+    {
+        feedItem.Rating = rated.R;
+    }
+}
 
-var input = string.Join("\n", feedItems.Select(x => x.Id + ", " + x.Title));
+feedItems = feedItems.OrderByDescending(x => x.Rating).ToList();
+WriteOutItems(feedItems);
 
-completionsOptions.Prompts.Add(
-"""
+void WriteOutItems(IList<FeedItem> feedItems) {
+    var table = new Table();
+    table.Border = TableBorder.HeavyHead;
+
+    // Add some columns
+    table.AddColumn(new TableColumn("Rating").Centered());
+    table.AddColumn("Title");
+    table.AddColumn("Link");
+
+    table.Columns[1].Padding(0, 10);
+
+    foreach (var feedItem in feedItems)
+    {
+        var color = feedItem.Rating switch
+        {
+            1 => "red",
+            2 => "yellow",
+            3 => "yellow",
+            4 => "green",
+            5 => "green",
+            _ => "white"
+        };
+        table.AddRow($"[{color}]{Markup.Escape(feedItem.Rating.ToString())}[/]", Markup.Escape(feedItem.Title), $"[link={feedItem.Link}]{feedItem.Id}[/]");
+    }
+
+    AnsiConsole.Write(table);
+}
+
+async Task<RatedArticleResponse[]> GetAIRatings(string azureOpenAiDeploymentName, IList<FeedItem> feedItems)
+{
+    var client = new OpenAIClient(new Uri(azureOpenAiServiceEndpoint), new AzureKeyCredential(azureOpenAiServiceKey));
+
+    var completionsOptions = new CompletionsOptions()
+    {
+        Temperature = 0,
+        MaxTokens = 2000,
+        NucleusSamplingFactor = 1,
+        FrequencyPenalty = 0,
+        PresencePenalty = 0,
+        GenerationSampleCount = 1,
+    };
+
+    var input = string.Join("\n", feedItems.Select(x => x.Id + ", " + x.Title));
+
+    completionsOptions.Prompts.Add(
+    """
 Rate on a scale of 1-5 how relevant each headline is to quantum computing software engineers. 
 Titles mentioning software, algorithms and error correction should be rated highly. Quantum computing hardware topics should be rated lower. Other quantum physics topics should get low rating. Produce result in JSON format as specified in the output example.
 
@@ -58,65 +102,21 @@ Titles mentioning software, algorithms and error correction should be rated high
 
 <Input>
 """ + "\n" + input + "\n" + "<Output>"
-);
+    );
 
-var completionsResponse = await client.GetCompletionsAsync(azureOpenAiDeploymentName, completionsOptions);
-if (completionsResponse.Value.Choices.Count == 0)
-{
-    Console.WriteLine("No completions found.");
-    return;
-}
-
-var preferredChoice = completionsResponse.Value.Choices[0];
-var rawJsonResponse = preferredChoice.Text.Trim();
-
-Console.WriteLine("Raw JSON response: " + rawJsonResponse);
-
-var results = JsonSerializer.Deserialize<RatedArticleResponse[]>(rawJsonResponse);
-foreach (var rated in results) 
-{
-    var feedItem = feedItems.FirstOrDefault(x => x.Id == rated.Id);
-    if (feedItem != null)
+    var completionsResponse = await client.GetCompletionsAsync(azureOpenAiDeploymentName, completionsOptions);
+    if (completionsResponse.Value.Choices.Count == 0)
     {
-        feedItem.Rating = rated.R;
-    }
-}
-
-feedItems = feedItems.OrderByDescending(x => x.Rating).ToList();
-
-foreach (var feedItem in feedItems)
-{
-    Console.WriteLine($"Rating: {feedItem.Rating} - {feedItem.Id} - {feedItem.Title}");
-}
-
-public class FeedItem
-{
-    public FeedItem(XElement item, XNamespace ns)
-    {
-        var title = item.Element(ns + "title")?.Value;
-        if (title != null)
-        {
-            Title = title[..title.LastIndexOf(". (arXiv:")];
-        }
-
-        var link = item.Element(ns + "link")?.Value;
-        Link = link;
-
-        Id = link.Split("/").Last();
-        Description = item.Element(ns + "description")?.Value;
-        Creator = item.Element(ns + "creator")?.Value;
+        Console.WriteLine("No completions found.");
+        return Array.Empty<RatedArticleResponse>();
     }
 
-    public string Id { get; set; }
-    public int Rating { get; set; }
-    public string Title { get; set; }
-    public string Link { get; set; }
-    public string Description { get; set; }
-    public string Creator { get; set; }
-}
+    var preferredChoice = completionsResponse.Value.Choices[0];
+    var rawJsonResponse = preferredChoice.Text.Trim();
 
-public class RatedArticleResponse
-{
-    public string Id { get; set; }
-    public int R { get; set; }
+    Console.WriteLine("Raw JSON response: " + rawJsonResponse);
+
+    var results = JsonSerializer.Deserialize<RatedArticleResponse[]>(rawJsonResponse);
+
+    return results;
 }
