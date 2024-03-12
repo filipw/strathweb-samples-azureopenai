30411b9e0145880628d20a1230c4a3929e433815
filipw <filip@strathweb.com>
2024-03-12 13:45:41 +0100

    empty function calling sample

---
+++ src/Strathweb.Samples.AzureOpenAI.FunctionCalling/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Text.Json;
+using System.Xml.Linq;
+using Azure;
+using Azure.AI.OpenAI;
+using Spectre.Console;
+using Strathweb.Samples.AzureOpenAI.Shared;
+
+Console.WriteLine("hello");
