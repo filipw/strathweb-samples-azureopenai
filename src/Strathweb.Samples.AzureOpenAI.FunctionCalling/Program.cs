fb4d72194e36bc7645dc38b2300fe57cd10a2ab7
filipw <filip@strathweb.com>
2024-04-02 17:25:24 +0200

    separate files

--- src/Strathweb.Samples.AzureOpenAI.FunctionCalling/Program.cs
+++ src/Strathweb.Samples.AzureOpenAI.FunctionCalling/Program.cs
@@ -3,7 +3,7 @@ using System.Text.Json;
 using Azure;
 using Azure.AI.OpenAI;
 using Spectre.Console;
-using Strathweb.Samples.AzureOpenAI.Shared;
+using Strathweb.Samples.AzureOpenAI.FunctionCalling;
 
 var azureOpenAiServiceEndpoint = Environment.GetEnvironmentVariable("AZURE_OPENAI_SERVICE_ENDPOINT") ??
                                  throw new Exception("AZURE_OPENAI_SERVICE_ENDPOINT missing");
@@ -204,66 +204,6 @@ void WriteOutItems(List<Entry> entries)
     }
 
     AnsiConsole.Write(table);
-}
-
-class ArxivClient
-{
-    private readonly OpenAIClient _client;
-    private readonly string _azureOpenAiDeploymentName;
-
-    public ArxivClient(OpenAIClient client, string azureOpenAiDeploymentName)
-    {
-        _client = client;
-        _azureOpenAiDeploymentName = azureOpenAiDeploymentName;
-    }
-    
-    public async Task<Feed> FetchPapers(SearchQuery searchQuery, DateTime date)
-    {
-        var query = searchQuery == SearchQuery.QuantumPhysics ? "cat:quant-ph" : "ti:\"quantum computing\"";
-        var feed = await ArxivHelper.FetchArticles(query, date.ToString("yyyyMMdd"));
-        return feed;
-    }
-    
-    public async Task<string> SummarizePaper(string paperId)
-    {
-        var feed = await ArxivHelper.FetchArticleById(paperId);
-        if (feed == null || feed.Entries.Count == 0)
-        {
-            return "Paper not found";
-        }
-        
-        if (feed.Entries.Count > 1)
-        {
-            return "More than one match for this ID!";
-        }
-
-        var prompt = $"Title: {feed.Entries[0].Title}{Environment.NewLine}Abstract: {feed.Entries[0].Summary}";
-        var systemPrompt = """
-        You are a summarization engine for ArXiv papers. You will take in input in the form of paper title and abstract, and summarize them in a digestible 1-2 sentence format.
-        Each summary should be a simple, plain text, separate paragraph.
-    """;
-        var completionsOptions = new ChatCompletionsOptions
-        {
-            Temperature = 0,
-            FrequencyPenalty = 0,
-            PresencePenalty = 0,
-            MaxTokens = 400,
-            DeploymentName = _azureOpenAiDeploymentName,
-            Messages =
-            {
-                new ChatRequestSystemMessage(systemPrompt),
-                new ChatRequestUserMessage(prompt)
-            }
-        };
-        var completionsResponse = await _client.GetChatCompletionsAsync(completionsOptions);
-        if (completionsResponse.Value.Choices.Count == 0)
-        {
-            return "No response available";
-        }
-
-        var preferredChoice = completionsResponse.Value.Choices[0];
-        return preferredChoice.Message.Content.Trim();
-    }
 }
 
 enum SearchQuery
